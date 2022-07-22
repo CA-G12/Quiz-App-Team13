eb4c283ec5c7ba2ef8b1a3e492dc9561e155bdcf
fadezak100 <fadyzaqut@gmail.com>
2022-07-22 16:59:56 +0300

    13-Add reset and back to main menu Functionality

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
 const users = []; 
-let userId = 0;
 
+initUser();
+
+function initUser(){
+    let userId = 0; 
+    if(localStorage.getItem('users') === null){
+         userId = 0;
+    } else{
+        let usersFromLocalStorage = JSON.parse(localStorage.getItem('users'));
+         userId = usersFromLocalStorage.length;
+    }
+    saveUser(userId); 
+}
+
+function saveUser(userId){
 document.getElementById('clickStart').addEventListener('click', function(){
     const userNameInput = document.getElementById('userNameInput');
     if(userNameInput.value === ""){
@@ -12,14 +25,21 @@ document.getElementById('clickStart').addEventListener('click', function(){
             userName: userName,
             score: 0
         }
+       
+        if(userId === 0){
+            users.push(user);
+            localStorage.setItem('users', JSON.stringify(users));
+        } else{
+            let usersFromLocalStorage = JSON.parse(localStorage.getItem('users'));
+            usersFromLocalStorage.push(user); 
+            localStorage.setItem('users', JSON.stringify(usersFromLocalStorage));
+        }
         userId += 1; 
-        users.push(user); 
         location.href = "./html/question.html?userId="+user.id; 
-        localStorage.setItem('users', JSON.stringify(users));
     }
 
 });
-
+}
 
 
 //Store Leaderboard in local storage
